7e7ddc42c916f25560bbf6ee7b61c4b18c75a294
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-06-20 13:34:08 -0700

    finished homework!

---
+++ functions_practice.py
@@ -0,0 +1,28 @@
+
+
+def hello():
+    print("Hello User!")
+
+def pack(x,y,z):
+    mylist = []
+    mylist.append(x)
+    mylist.append(y)
+    mylist.append(z)
+    return mylist
+
+def eat_lunch(lunch_list):
+    if lunch_list.length == 0:
+        print("My lunchbox is empty!")
+    else:
+        for item in lunch_list:
+        if item = lunch_list[0]:
+            print("First I eat",item)
+        elif:
+            print("Next I eat",item)
+
+hello()
+print(pack("a","b","c"))
+print(pack(1,2,3))
+eat_lunch([])
+eat_lunch(["sandwich"])
+eat_lunch(["apple","banana","sandwich","cookie"])
